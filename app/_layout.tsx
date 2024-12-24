fe8fb7ba26d39ebb0777d32f66e849ea8f1b27b7
mine <bertin.m2001@gmail.com>
2024-12-24 11:28:33 +0200

    changed logos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,11 @@ export default function RootLayout() {
     <ApolloProvider client={client}>
       <Stack
         screenOptions={{
-          headerShown: false,
+          headerShown: true,
         }}
       >
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false  }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
